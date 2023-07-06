13d11f22d22db48fb05ff58cd46dc5f28f3f1f58
wym0913 <122755552+wym0913@users.noreply.github.com>
2023-07-07 06:43:41 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import "./styles/highlight.scss";
 import { getClientConfig } from "./config/client";
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "IPRJ GPT",
+  description: "Your personal IPRJGPT Chat Bot.",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -17,7 +17,7 @@ export const metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "IPRJ GPT",
     statusBarStyle: "default",
   },
 };
